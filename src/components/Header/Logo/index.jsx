232a30efb2a15254528601ed12e2d61b1c1b3c6f
Juan Manuel Ibanez <juanmanuelibanez@Juans-MacBook-Pro.local>
2020-05-17 01:01:23 -0300

    resets when click on Marvel log on the header

--- src/components/Header/Logo/index.jsx
+++ src/components/Header/Logo/index.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import marvelLogo from './Marvel.png';
 import { LogoContainer } from './styles';
 
+import { InputContext } from '../../../context';
+
 const Logo = () => {
 
-    return (
-        <LogoContainer>
-            <img src={marvelLogo} alt="Smiley face" height="50" width="100" />
-        </LogoContainer>
-    )
+  const { setInput } = useContext(InputContext);
+
+  return (
+    <LogoContainer onClick={() => setInput('')}>
+      <img src={marvelLogo} alt="Smiley face" height="50" width="100" />
+    </LogoContainer>
+  )
 }
 
 export default Logo;
